3d5df46989646036b84430cbdefe2ea3aa7f1577
colecharb <40375497+colecharb@users.noreply.github.com>
2020-12-10 18:45:23 -0500

    Add files via upload

--- graphsFinal.py
+++ graphsFinal.py
@@ -42,31 +42,74 @@ def compose(c1, c2, p=False):
 
 
 def main():
-    print("\n#####","r =", r, "#####\n")
-
-    # Set of all nontrivial matchings that can be expressed as an algebraic r-cycle
-    # (so as to form a perfect pair with the trivial matching, represented by the identity permutation on Sr)
-    ms = []
-    for n in range(1,r):
-        m = gen(n)
-        if len(m) == r:
-            ms.append(m)
-
-    print("%i MATCHING CANDIDATES:" %len(ms))
-    for m in ms: print(m)
-    print()
 
 
-    # To store compositions of all pairs (x, y^-1) from list ms
-    cs = []
-    for i in range(len(ms)):
-        for j in range(i+1, len(ms)):
-            m1, m2 = copy(ms[i]), copy(ms[j])
-            m2.reverse()
-            cs.append(compose(m1, m2))
+    # print("\n#####","r =", r, "#####\n")
 
-    print("ALL COMPOSITIONS OF PAIRS FROM ABOVE:")
-    for c in cs: print(c)
+    # # Set of all nontrivial matchings that can be expressed as an algebraic r-cycle
+    # # (so as to form a perfect pair with the trivial matching, represented by the identity permutation on Sr)
+    # ms = []
+    # for n in range(1,r):
+    #     m = gen(n)
+    #     if len(m) == r:
+    #         ms.append(m)
+
+    # print("%i MATCHING CANDIDATES:" %len(ms))
+    # for m in ms: print(m)
+    # print()
+
+
+    # # To store compositions of all pairs (x, y^-1) from list ms
+    # cs = []
+    # for i in range(len(ms)):
+    #     for j in range(i+1, len(ms)):
+    #         m1, m2 = copy(ms[i]), copy(ms[j])
+    #         m2.reverse()
+    #         cs.append(compose(m1, m2))
+
+    # print("ALL COMPOSITIONS OF PAIRS FROM ABOVE:")
+    # for c in cs: print(c)
+
+
+
+    # Very important for the for loop:
+    global r
+
+    for ri in range(100):
+        perf = True
+        r = ri
+        # print("\n#####","r =", r, "#####\n")
+
+
+        # stores all matchings of length r
+        ms = []
+        for n in range(1,r):
+            m = gen(n)
+            if len(m) == r:
+                ms.append(m)
+
+        if len(ms) < (r-1):
+            continue
+
+        
+        # To store compositions of all pairs (x, y^-1) from list ms
+        cs = []
+        for i in range(len(ms)):
+            for j in range(i+1, len(ms)):
+                m1, m2 = copy(ms[i]), copy(ms[j])
+                m2.reverse()
+                cs.append(compose(m1, m2))
+        
+        for c in cs:
+            if len(c) < r:
+                perf = False
+
+        # if made it this far, K_r,r has a perfect 1-factorization!
+        if perf: print(r)
+
+
+
+
 
     return
 
